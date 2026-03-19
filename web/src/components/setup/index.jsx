e47ba4f8f26ae08f8e753961fa645ed8e9fd7977
阮 <zhongruan0522@gmail.com>
2026-03-19 11:03:50 +0000

    fix: 移除 setup/index.jsx 中对已删除 UsageModeStep 的导出引用

--- web/src/components/setup/index.jsx
+++ web/src/components/setup/index.jsx
@@ -25,5 +25,4 @@ export { default as StepNavigation } from './components/StepNavigation';
 // 步骤组件导出
 export { default as DatabaseStep } from './components/steps/DatabaseStep';
 export { default as AdminStep } from './components/steps/AdminStep';
-export { default as UsageModeStep } from './components/steps/UsageModeStep';
 export { default as CompleteStep } from './components/steps/CompleteStep';
