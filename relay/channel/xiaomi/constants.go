b19eaeae63ce0661c1ca2e57707d6ca9d177c873
zhongruan0522 <zhongruan0522@gmail.com>
2026-04-29 00:58:09 +0000

    feat: add MiMo V2.x models to Xiaomi channel model list

--- relay/channel/xiaomi/constants.go
+++ relay/channel/xiaomi/constants.go
@@ -2,6 +2,14 @@ package xiaomi
 
 var ModelList = []string{
 	"mimo-vl-01",
+	"mimo-v2.5-pro",
+	"mimo-v2.5",
+	"mimo-v2.5-tts-voiceclone",
+	"mimo-v2.5-tts-voicedesign",
+	"mimo-v2.5-tts",
+	"mimo-v2-pro",
+	"mimo-v2-omni",
+	"mimo-v2-tts",
 }
 
 var ChannelName = "xiaomi"
